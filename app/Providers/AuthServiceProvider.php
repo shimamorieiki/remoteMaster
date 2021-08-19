3f762db31e1acdfccfb356855238c018f0722b15
shimamorieiki <aifore.2270@gmail.com>
2021-08-19 11:23:05 +0900

    import sanctum

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
-
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -26,6 +24,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes();
+        // 
+
     }
 }
