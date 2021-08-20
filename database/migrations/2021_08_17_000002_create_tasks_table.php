62b0b578f8b488f1087594055d03277022f2085c
shimamorieiki <aifore.2270@gmail.com>
2021-08-20 12:58:48 +0900

    テーブル構成変更

--- database/migrations/2021_08_17_000002_create_tasks_table.php
+++ database/migrations/2021_08_17_000002_create_tasks_table.php
@@ -15,7 +15,7 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->integer('grade_id')->unsigned()->constrained();
             $table->integer('genre_id')->unsigned()->constrained();
             $table->text('description');
