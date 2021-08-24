f892137086e02b0c5ad43f5250f01a55953c0480
shimamorieiki <aifore.2270@gmail.com>
2021-08-24 11:24:14 +0900

    change; task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'grade_id',
+        'genre_id',
+        'description',
+        'is_positive_check'
+    ];
 }
