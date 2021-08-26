e92bfdbefd65048d382cf1840f458f1fd62f6ce9
shimamorieiki <aifore.2270@gmail.com>
2021-08-26 12:29:27 +0900

    feat: VoteControllerTest

--- tests/Feature/VoteControllerTest.php
+++ tests/Feature/VoteControllerTest.php
@@ -6,8 +6,9 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
+use App\Models\User;
 
-class UserControllerTest extends TestCase
+class VoteControllerTest extends TestCase
 {
     use DatabaseTransactions;
     public function setUp(): void
@@ -20,85 +21,122 @@ class UserControllerTest extends TestCase
         // インサートするテーブルをこっちで決めればいいのか
         // テーブルだけ作っておいてシーディングをここからする
         
-        // ログインする
+        // テストログイン用の管理者ユーザー作成
+        $email = 'testadmin@co.jp';
+        $password = 'pass';
         
-        
-        
-        // ログイン情報を利用してgetリクエスト飛ばす
-        $response = $this->get(route('users.get_winner'));
-        // レスポンスの検証
-        // 勝者が存在するとき
-        $response->assertOk() # ステータスコードが 200
-            ->assertJsonCount(1) # レスポンスの配列の件数がtasksテーブルのレコード数
-            // レスポンスで返ってきたものが全て一致する
-            ->assertJsonFragment([ # レスポンスJSON に以下の値を含む
-                'name' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
-                'voting_number' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
-            ]);
+        // 同名のユーザを削除
+        User::where('email', $email)->delete();
 
-        // 勝者が存在しないとき
-        $response->assertOk() # ステータスコードが 200
-            ->assertJsonCount(1) # レスポンスの配列の件数がtasksテーブルのレコード数
-            // レスポンスで返ってきたものが全て一致する
-            ->assertJsonFragment([ # レスポンスJSON に以下の値を含む
-                'name' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
-                'voting_number' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
-            ]);
-
-        // 回答を確認する権限がないとき
-        $response->assertOk(); # ステータスコードが 200
-    }
-
-    public function post_completed_task()
-    {
+        // テストユーザ作成
+        $user_admin = User::create([
+            'name' => 'TestAdm',
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role_id' => 1,
+        ]);
         
-        // ログインする
+        // テストログイン用の一般ユーザー作成
+        $email = 'testgeneral@co.jp';
+        $password = 'word';
+        
+        // 同名のユーザを削除
+        User::where('email', $email)->delete();
+
+        // テストユーザ作成
+        $user_general = User::create([
+            'name' => 'Testgen',
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role_id' => 2,
+        ]);
+                
         
         // ログイン情報を利用してgetリクエスト飛ばす
-
-        $data1 = [ # 登録用のデータ
-            'task_id' => 1,
-            'is_task_checked' => FALSE
-        ];
-        
-        $data2 = [ # 登録用のデータ
-            'task_id' => 1,
-            'is_task_checked' => TRUE
-        ];
-
-
-        // 投票する前
-        // まだチェックしてなくてチェックしようとする
-        $response1 = $this->post(route('users.get_user_info'), $data1);
+        // 管理者ユーザなら結果を取得できる
+        $response1 = $this->actingAs($user_admin)->get('api/lottery');
         $response1->assertOk();
 
-        // チェックしていてチェックしようとする
-        $response2 = $this->post(route('users.get_user_info'), $data1);
-        $response2->assertOk();
+        // 取得した結果が正しいかをこれから見る
+
+        // 一般ユーザなら結果を取得できない
+        $response2 = $this->actingAs($user_general)->get('api/lottery');
+        $response2->assertStatus(400);
+
+        // レスポンスの検証
+        // 勝者が存在するとき
+        // $response->assertOk() # ステータスコードが 200
+        //     ->assertJsonCount(1) # レスポンスの配列の件数がtasksテーブルのレコード数
+        //     // レスポンスで返ってきたものが全て一致する
+        //     ->assertJsonFragment([ # レスポンスJSON に以下の値を含む
+        //         'name' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
+        //         'voting_number' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
+        //     ]);
+
+        // // 勝者が存在しないとき
+        // $response->assertOk() # ステータスコードが 200
+        //     ->assertJsonCount(1) # レスポンスの配列の件数がtasksテーブルのレコード数
+        //     // レスポンスで返ってきたものが全て一致する
+        //     ->assertJsonFragment([ # レスポンスJSON に以下の値を含む
+        //         'name' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
+        //         'voting_number' => '', // ここでシーディングしていれば答えの人間と数字を決め打ちできる
+        //     ]);
+    }
+
+    public function test_vote()
+    {
         
-        // チェックしていて外そうとする
-        $response3 = $this->post(route('users.get_user_info'), $data2);
-        $response3->assertOk();
-
-        // チェックしていなくてチェックを外そうとする
-        $response4 = $this->post(route('users.get_user_info'), $data2);
-        $response4->assertOk();
-
-        // 投票後
-        // まだチェックしてなくてチェックしようとする
-        $response5 = $this->post(route('users.get_user_info'), $data1);
-        $response5->assertOk();
-
-        // チェックしていて外そうとする
-        $response6 = $this->post(route('users.get_user_info'), $data1);
-        $response6->assertOk();
-
-        // チェックしていてチェックしようとする
-        $response7 = $this->post(route('users.get_user_info'), $data2);
-        $response7->assertOk();
+        $email = 'testadmin@co.jp';
+        $password = 'pass';
         
-        // チェックしていなくてチェックを外そうとする
-        $response8 = $this->post(route('users.get_user_info'), $data2);
-        $respons8->assertOk();
+        // 同名のユーザを削除
+        User::where('email', $email)->delete();
+
+        // テストユーザ作成
+        $user_admin = User::create([
+            'name' => 'TestAdm',
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role_id' => 1,
+        ]);
+
+        // テストログイン用の一般ユーザー作成
+        $email = 'testgeneral@co.jp';
+        $password = 'word';
+        
+        // 同名のユーザを削除
+        User::where('email', $email)->delete();
+
+        // テストユーザ作成1
+        $user_general = User::create([
+            'name' => 'Testgen',
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role_id' => 2,
+        ]);
+
+        // 管理者権限ユーザは投票できない
+        $response1 = $this->actingAs($user_admin)->post('api/lottery',['voting_number'=>$user_admin->id]);
+        $response1->assertStatus(400);
+
+        // 一般ユーザは投票できる
+        
+        // // 投票できた
+        // $response2 = $this->actingAs($user_general)->post('api/lottery',['voting_number'=>$user_general->id]);
+
+
+        // // バリデーションエラーに引っかかる
+        // $response2 = $this->actingAs($user_general)->post('api/lottery',['voting_number'=>$user_general->id]);
+        
+        // タスクを15個以上こなしていない
+        $response2 = $this->actingAs($user_general)->post('api/lottery',['voting_number'=>$user_general->id]);
+        $response1->assertStatus(400);
+
+        // // ネガティブチェックがある
+        // $response2 = $this->actingAs($user_general)->post('api/lottery',['voting_number'=>$user_general->id]);
+
+        // // すでに投票している
+        // $response2 = $this->actingAs($user_general)->post('api/lottery',['voting_number'=>$user_general->id]);
+
     }
 }
