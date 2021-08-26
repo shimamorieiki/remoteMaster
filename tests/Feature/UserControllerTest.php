9694d905f1439ca6d420fd9211219d9e02d220e1
shimamorieiki <aifore.2270@gmail.com>
2021-08-26 12:11:09 +0900

    feat: UserControllerTest

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -5,42 +5,70 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\User;
+use App\Models\Task;
+use App\Models\Vote;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
     use DatabaseTransactions;
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
 
     public function test_get_user_info()
     {
         
-        // ログインする
+        // テストログイン用のユーザー作成
+        $email = 'testadmin@example.com';
+        $password = '12345678';
         
-        // ログイン情報を利用してgetリクエスト飛ばす
-        $response = $this->get(route('users.get_user_info'));
+        // 同名のユーザを削除
+        User::where('email', $email)->delete();
 
-        // Taskテーブルのレコード数
-        // $tasks_num = 
+        // テストユーザ作成
+        $user = User::create([
+            'name' => 'TestAdmin',
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role_id' => 1,
+        ]);
+        
+        
+        // ログインする
+        $response = $this->actingAs($user)
+                        ->get('api/user');
+        
+        // Taskの個数を取得する
+        $tasks_num = Task::select()->count();
 
-        // レスポンスの検証
-        $response->assertOk() # ステータスコードが 200
-            ->assertJsonCount(1 /* $tasks_num */) # レスポンスの配列の件数がtasksテーブルのレコード数
-            // レスポンスで返ってきたものが全て一致する
-            ->assertJsonFragment([ # レスポンスJSON に以下の値を含む
-                'email' => 'user1@example.com',
-            ]);
+        $response->assertOk() // ステータスコードが 200
+                ->assertJsonCount($tasks_num); // Taskテーブルのレコード数と一致
+        
+        // 戻り値が正しいかどうかを判断する。
+        // Tasksの個数を全部取得できていれば問題ない気もする。
+        // とりあえず中身を見るのは後回しにする。
+
+        // 作ったユーザを削除する
+        User::where('email', $email)->delete();
     }
 
-    public function post_completed_task()
+    public function test_post_completed_task()
     {
+
+        // テストログイン用のユーザー作成
+        $email = 'testadmin@example.com';
+        $password = '12345678';
         
-        // ログインする
+        // 同名のユーザを削除
+        User::where('email', $email)->delete();
+
+        // テストユーザ作成
+        $user = User::create([
+            'name' => 'TestAdmin',
+            'email' => $email,
+            'password' => bcrypt($password),
+            'role_id' => 1,
+        ]);
         
-        // ログイン情報を利用してgetリクエスト飛ばす
 
         $data1 = [ # 登録用のデータ
             'task_id' => 1,
@@ -55,36 +83,45 @@ class UserControllerTest extends TestCase
 
         // 投票する前
         // まだチェックしてなくてチェックしようとする
-        $response1 = $this->post(route('users.get_user_info'), $data1);
+        $response1 = $this->actingAs($user)->post('api/user',$data1);
         $response1->assertOk();
 
         // チェックしていてチェックしようとする
-        $response2 = $this->post(route('users.get_user_info'), $data1);
-        $response2->assertOk();
+        $response2 = $this->actingAs($user)->post('api/user',$data1);
+        $response2->assertStatus(400);
         
         // チェックしていて外そうとする
-        $response3 = $this->post(route('users.get_user_info'), $data2);
+        $response3 = $this->actingAs($user)->post('api/user', $data2);
         $response3->assertOk();
 
         // チェックしていなくてチェックを外そうとする
-        $response4 = $this->post(route('users.get_user_info'), $data2);
-        $response4->assertOk();
+        $response4 = $this->actingAs($user)->post('api/user', $data2);
+        $response4->assertStatus(400);
+
+        // 投票する
+        Vote::create([
+            'user_id' => $user->id,
+            'voting_number' => 100
+        ]);
 
         // 投票後
         // まだチェックしてなくてチェックしようとする
-        $response5 = $this->post(route('users.get_user_info'), $data1);
-        $response5->assertOk();
+        $response5 = $this->actingAs($user)->post('api/user', $data1);
+        $response5->assertStatus(400);
 
         // チェックしていて外そうとする
-        $response6 = $this->post(route('users.get_user_info'), $data1);
-        $response6->assertOk();
+        $response6 = $this->actingAs($user)->post('api/user', $data1);
+        $response6->assertStatus(400);
 
         // チェックしていてチェックしようとする
-        $response7 = $this->post(route('users.get_user_info'), $data2);
-        $response7->assertOk();
+        $response7 = $this->actingAs($user)->post('api/user', $data2);
+        $response7->assertStatus(400);
         
         // チェックしていなくてチェックを外そうとする
-        $response8 = $this->post(route('users.get_user_info'), $data2);
-        $respons8->assertOk();
+        $response8 = $this->actingAs($user)->post('api/user', $data2);
+        $response8->assertStatus(400);
+
+        // 同名のユーザを削除
+        User::where('email', $email)->delete();
     }
 }
